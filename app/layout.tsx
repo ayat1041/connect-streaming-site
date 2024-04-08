65e5789a0584fc4499ad437ead55c17c3f236927
ayat1041 <ayat.dihan6593@gmail.com>
2024-04-08 15:00:48 +0600

    project initialized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+import Link from "next/link";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +19,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider appearance={{ baseTheme: dark }}>
+      <html lang="en">
+        <body className={`${inter.className} h-max py-8 min-h-screen`}>
+          <ThemeProvider
+          attribute="class"
+          forcedTheme="dark"
+          storageKey="connect-theme"
+          >
+          <nav className="flex font-bold text-slate-50 gap-12 items-center justify-start lg:px-8 pb-8 pt-2">
+            <Link href="/">Home</Link>
+            <Link href="sign-in/">Login</Link>
+            <Link href="sign-up/">Sign Up</Link>
+          </nav>
+          {children}
+          </ThemeProvider>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
